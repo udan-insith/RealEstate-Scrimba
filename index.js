5585327ea18ca1c6cc0c561e643b383276a893dc
udan-insith <udaninsith23613@gmail.com>
2026-02-20 10:30:27 +0530

    Got it working
    Main functions are working without any trouble now .

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function getPropertyHtml() {
+import propertyForSaleArr from "./properties/propertyForSaleArr.js"
 
+function getPropertyHtml(propertyArr) {
+    return propertyArr.map(property => {
+        const {propertyLocation, priceGBP, roomM2, comment, image } = property
+        return `
+        <section class="card">
+            <img src="/images/${image}">
+            <div class="card-right">
+                <h2 id="location-el">${propertyLocation}</h2>
+                <h3 id="price-el">${priceGBP}</h3>
+                <p id="comment-el">${comment}</p>
+                <h3 id="size-el">TOTAL SIZE IN SQUARE METERS m&sup2;</h3>
+            </div>
+        </section>`
+    })
 }
 
-document.getElementById('container').innerHTML = getPropertyHtml()
+document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -12,14 +12,14 @@ const propertyForSaleArr = [
         roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
         comment: 'Astonishing view with a modern finish!',
         image: 'home2.jpg'
-    }
+    },
     {
         propertyLocation: 'Beach, Brighton',
         priceGBP: 420000,
         roomsM2: [5],
         comment: 'Beautiful interior and a spacious room.',
         image: 'home3.jpg'
-    }
+    },
     {
         propertyLocation: 'Highland, Scotland',
         priceGBP: 550000,
@@ -28,3 +28,5 @@ const propertyForSaleArr = [
         image: 'home6.jpg'
     }
 ]
+
+export default propertyForSaleArr
